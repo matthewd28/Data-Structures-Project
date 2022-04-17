4901d4b3fb75e07cb2145be8531ddf2fb9cb9946
Matthew Davis <87373031+matthewd28@users.noreply.github.com>
2022-04-17 13:02:12 -0400

    Update main.cpp
    Final main before implementing sorting algorithms (barring any bugs).

--- main.cpp
+++ main.cpp
@@ -14,14 +14,17 @@ int main() {
 	//So basically we're going to have "relevancy points," where each criteria that is met by a college (based upon our inputs) will determine a quick sort or merged sorted vector of the best
 	//colleges to pick.
 
-	//When inputting an SAT score, relevant colleges will be centered around the 50th percentile score, ranging from 25th to 75th percentiles
-
 	ifstream degreeData("Most-Recent-Cohorts-Field-of-Study.csv");
 	ifstream collegeData("COP3530 Trimmed Data.csv");
 
-	//Finding the types of degrees
-	set<string> degrees;
-	map<string, vector<Degree>> schoolToDegree;
+	//ALl possible degree programs
+	set<string> listDegrees;
+
+	//The degrees possible for each college
+	map<string, vector<Degree>> collegeDegreeData;
+
+	//College data pertaining to SAT, admission rate, etc.
+	vector<College> colleges;
 
 	//This will take a minute or two to parse all of the data
 	string line;
@@ -42,8 +45,8 @@ int main() {
 		replace(degreeType.begin(), degreeType.end(), '.', ',');
 
 		//Non-unique degrees show up many times for different schools, so a set works best
-		degrees.insert(degreeProgram);
-		schoolToDegree[institution].push_back(Degree(institution, publicPrivate, degreeProgram, degreeType));
+		listDegrees.insert(degreeProgram);
+		collegeDegreeData[institution].push_back(Degree(institution, publicPrivate, degreeProgram, degreeType));
 	}
 
 	/*for (auto x : degrees) {
@@ -59,43 +62,53 @@ int main() {
 		}
 
 		string institution, city, state, admissionRate, satvr25, satvr75, satmt25, satmt75, satwr25, satwr75, satAvg, undergraduates, costAttendance;
-		int admit, satr25, satr75, satm25, satm75, satw25, satw75, satAverage, numUndergrad, numCost;
+		int numUndergrad, numCost;
+		float admit, satAverage;
 		stringstream ss(line);
 		
 
 		//We only want to consider the colleges that were found in the field of study file
 		getline(ss, institution, ',');
-		if (schoolToDegree.find(institution) == schoolToDegree.end())
+		if (collegeDegreeData.find(institution) == collegeDegreeData.end())
 			continue;
 
 		getline(ss, city, ',');
+		if (city == "NULL")
+			continue;
+
 		getline(ss, state, ',');
+		if (state == "NULL")
+			continue;
 
 		//Converting values to int
 		getline(ss, admissionRate, ',');
-		admit = stoi(admissionRate);
+		if (admissionRate == "NULL")
+			continue;
+
+		admit = stof(admissionRate);
 
 		//A lot of schools are missing 25th and 75th percentile sat data,
 		//so we will make an assumption that a school is valid if their sat requirement is less than x amount greater than the user's
 		getline(ss, satAvg, ',');
-		satAverage = stoi(satAvg);
+		if (satAvg == "NULL")
+			continue;
+
+		satAverage = stof(satAvg);
 
 		getline(ss, undergraduates, ',');
+		if (undergraduates == "NULL")
+			continue;
+
 		numUndergrad = stoi(undergraduates);
 
 		getline(ss, costAttendance, ',');
+		if (costAttendance == "NULL")
+			continue;
+
 		numCost = stoi(costAttendance);
 
-
-
-
+		colleges.push_back(College(institution, city, state, admit, satAverage, numUndergrad, numCost));
 	}
-
-
-
-
-
-
 
 
 
