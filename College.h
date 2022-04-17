abc0c53b439c309db157c2034e5d28e67d6264dc
Matthew Davis <87373031+matthewd28@users.noreply.github.com>
2022-04-16 21:23:41 -0400

    Update College.h

--- College.h
+++ College.h
@@ -8,12 +8,12 @@ struct College {
 
 	string institution, city, state;
 	float admissionRate, satAverage;
-	int numUndergraduates, costAttendance;
+	int numUndergraduates, costAttendance, sat25, sat50, sat75;
 
 	vector<Degree> degrees;
 
 
-	College(string institution_, string city_, string state_, float admissionRate_, float satAverage_, int numUndergraduates_, int costAttendance_) {
+	College(string institution_, string city_, string state_, float admissionRate_, float satAverage_, int sat25, int sat50, int sat75, int numUndergraduates_, int costAttendance_) {
 		institution = institution_;
 		city = city_;
 		state = state_;
