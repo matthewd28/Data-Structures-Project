d6a357fa9f89a592ff54c12c28993e4da9642477
nikhil-a-pandya <73561932+nikhil-a-pandya@users.noreply.github.com>
2022-04-20 14:32:30 -0400

    Update College.h
    Added relevancy points

--- College.h
+++ College.h
@@ -9,6 +9,7 @@ struct College {
 	string institution, city, state;
 	float admissionRate, satAverage;
 	int numUndergraduates, costAttendance, sat25, sat50, sat75;
+	int relevancyPts;
 
 	vector<Degree> degrees;
 	
@@ -16,7 +17,7 @@ struct College {
 
 	}
 
-	College(string institution_, string city_, string state_, float admissionRate_, float satAverage_, int numUndergraduates_, int costAttendance_) {
+	College(string institution_, string city_, string state_, float admissionRate_, float satAverage_, int numUndergraduates_, int costAttendance_, int relevancyPts_) {
 		institution = institution_;
 		city = city_;
 		state = state_;
@@ -24,5 +25,6 @@ struct College {
 		satAverage = satAverage_;
 		numUndergraduates = numUndergraduates_;
 		costAttendance = costAttendance_;
+		relevancyPts = relevancyPts_;
 	}
 };
